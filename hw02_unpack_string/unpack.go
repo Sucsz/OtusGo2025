9ce1933ff07f6daaf8c458ca83754acc25969a12
Sucsz <VladimirSemykin.Other@yandex.ru>
2025-02-22 20:32:39 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,35 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var builder strings.Builder
+
+	for i := 0; i < len(str); {
+		char, size := utf8.DecodeRuneInString(str[i:])
+		// strconv.Atoi(...) делает код не очень читаемым из-за принципа возвращения ошибки
+		// Правда unicode.IsDigit(...) кушает и не арабские цифры, как цифры
+		if unicode.IsDigit(char) {
+			// если символ цифра, то она или первая в строке или предыдущий символ тоже цифра
+			return "", ErrInvalidString
+		}
+		if i+size < len(str) {
+			// следующий символ существует
+			nextChar, nextSize := utf8.DecodeRuneInString(str[i+size:])
+			if unicode.IsDigit(nextChar) {
+				builder.WriteString(strings.Repeat(string(char), int(nextChar-'0')))
+				i += size + nextSize
+				continue
+			}
+		}
+		builder.WriteRune(char)
+		i += size
+	}
+	return builder.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -18,6 +18,14 @@ func TestUnpack(t *testing.T) {
 		{input: "aaa0b", expected: "aab"},
 		{input: "🙃0", expected: ""},
 		{input: "aaф0b", expected: "aab"},
+		{input: "a\nb\n3c3", expected: "a\nb\n\n\nccc"},
+		{input: "a1b", expected: "ab"},
+		{input: "-5", expected: "-----"},
+		{input: "a-3", expected: "a---"},
+		{input: "a b", expected: "a b"},
+		{input: "a 5b", expected: "a     b"},
+		{input: "a\t2b", expected: "a\t\tb"},
+		{input: "!2a2", expected: "!!aa"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -26,7 +34,6 @@ func TestUnpack(t *testing.T) {
 	}
 
 	for _, tc := range tests {
-		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
@@ -36,9 +43,8 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "a03", "a00"}
 	for _, tc := range invalidStrings {
-		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
